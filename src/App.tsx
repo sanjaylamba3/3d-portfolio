e140d34b0313aa47bf2c76602efa78c3c6f7bee3
Sanjay <sanjaylamba3@gmail.com>
2025-05-11 16:27:39 +0530

    Initital commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+    // const handleClick = () => {
+    //     console.log("ee");
+    // };
+    return (
+        <main>
+            <div>
+                <h1 className="text-3xl">Welcome to my 3d portfolio</h1>
+            </div>
+        </main>
+    );
+};
+
+export default App;
