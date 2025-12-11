cf9422e597d8c6d1091fdf882661b56da8d5951a
sanjaylamba3 <sanjaylamba3@gmail.com>
2025-12-12 02:22:46 +0530

    refactor: update HeroExperience component and add HeroLights and Room components

--- src/components/hero-models/hero-experience.tsx
+++ src/components/hero-models/hero-experience.tsx
@@ -1,13 +1,14 @@
-import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-
+import { Canvas } from "@react-three/fiber";
+import useMediaQuery from "react-responsive";
+import HeroLights from "./hero-lights";
+import { Room } from "./room";
 
 const HeroExperience = () => {
-  const isTablet = false;
+  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-      <ambientLight intensity={0.2} color="#1a1a40" />
-      <directionalLight position={[5, 5, 5]} intensity={5} />
       <OrbitControls
         enablePan={false}
         enableZoom={!isTablet}
@@ -16,10 +17,18 @@ const HeroExperience = () => {
         minPolarAngle={Math.PI / 5}
         maxPolarAngle={Math.PI / 2}
       />
-      <mesh>
+      {/* <mesh>
         <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="teal" />
-      </mesh>
+        <meshStandardMaterial color="violet" />
+      </mesh> */}
+      <HeroLights />
+      <group
+        scale={isMobile ? 0.1 : 1}
+        position={[0, -3.5, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
+      >
+        <Room />
+      </group>
     </Canvas>
   );
 };
